d415b32a8d87aaaef5936f4949d77e1cacdaf8e7
m0trex <timofeyalisov@gmail.com>
2026-06-25 11:40:48 +0300

    func for vector

--- alisov.timofey/U1/main.cpp
+++ alisov.timofey/U1/main.cpp
@@ -21,6 +21,41 @@ namespace alisov
       delete[] data;
     }
   };
+
+  template < class T >
+  void init(Vector< T > &vec)
+  {
+    vec.data = nullptr;
+    vec.size = 0;
+    vec.cap = 0;
+  }
+
+  template < class T >
+  void reserve(Vector< T > &vec, size_t new_cap)
+  {
+    if (new_cap <= vec.cap) {
+      return;
+    }
+    T *new_data = new T[new_cap];
+    for (size_t i = 0; i < vec.size; ++i) {
+      new_data[i] = vec.data[i];
+    }
+    delete[] vec.data;
+    vec.data = new_data;
+    vec.cap = new_cap;
+  }
+
+  template < class T >
+  void push_back(Vector< T > &vec, const T &val)
+  {
+    if (vec.size >= vec.cap) {
+      size_t new_cap = vec.cap == 0 ? 4 : vec.cap * 2;
+      reserve(vec, new_cap);
+    }
+    vec.data[vec.size] = val;
+    ++vec.size;
+  }
+
 }
 
 int main(int argc, char *argv[])
