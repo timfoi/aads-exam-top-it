0c8b72bf3df8dcf840c97c81a6e9a0be0bf133a8
m0trex <timofeyalisov@gmail.com>
2026-06-25 13:35:22 +0300

    add sort and main

--- alisov.timofey/U2/main.cpp
+++ alisov.timofey/U2/main.cpp
@@ -75,6 +75,23 @@ namespace alisov
     return true;
   }
 
+  template < class T, class Compare >
+  void bubble_sort(Vector< T > &vec, Compare comp)
+  {
+    if (vec.size == 0) {
+      return;
+    }
+    for (size_t i = 0; i < vec.size - 1; ++i) {
+      for (size_t j = 0; j < vec.size - i - 1; ++j) {
+        if (comp(vec.data[j + 1], vec.data[j])) {
+          T temp = vec.data[j];
+          vec.data[j] = vec.data[j + 1];
+          vec.data[j + 1] = temp;
+        }
+      }
+    }
+  }
+
   bool parse_person(const std::string &line, Person &out_person, bool &is_empty)
   {
     is_empty = false;
@@ -114,7 +131,103 @@ namespace alisov
     out_person.info = line.substr(info_start, info_end - info_start + 1);
     return true;
   }
+
+  bool parse_meet(const std::string &line, Meet &out_meet)
+  {
+    if (line.empty()) {
+      return false;
+    }
+    const size_t f_space = line.find_first_not_of(" \t\r\n");
+    if (f_space == std::string::npos || line[f_space] < '0' || line[f_space] > '9') {
+      return false;
+    }
+    const size_t id1_end = line.find_first_of(" \t\r\n", f_space);
+    if (id1_end == std::string::npos) {
+      return false;
+    }
+
+    const size_t id2_start = line.find_first_not_of(" \t\r\n", id1_end);
+    if (id2_start == std::string::npos || line[id2_start] < '0' || line[id2_start] > '9') {
+      return false;
+    }
+    const size_t id2_end = line.find_first_of(" \t\r\n", id2_start);
+    if (id2_end == std::string::npos) {
+      return false;
+    }
+
+    const size_t dur_start = line.find_first_not_of(" \t\r\n", id2_end);
+    if (dur_start == std::string::npos || line[dur_start] < '0' || line[dur_start] > '9') {
+      return false;
+    }
+    const size_t dur_end = line.find_first_of(" \t\r\n", dur_start);
+    const std::string dur_str =
+        (dur_end == std::string::npos) ? line.substr(dur_start) : line.substr(dur_start, dur_end - dur_start);
+
+    size_t pb = 0;
+    out_meet.id1 = static_cast< size_t >(std::stoull(line.substr(f_space, id1_end - f_space)));
+    out_meet.id2 = static_cast< size_t >(std::stoull(line.substr(id2_start, id2_end - id2_start)));
+    out_meet.duration = static_cast< size_t >(std::stoull(dur_str, &pb));
+
+    if (pb != dur_str.length()) {
+      return false;
+    }
+    return true;
+  }
 }
 
-int main()
-{}
+int main(int argc, char *argv[])
+{
+  if (argc > 4) {
+    return 0;
+  }
+  std::string in_file = "";
+  std::string out_file = "";
+  std::string data_file = "";
+  bool has_in = false;
+  bool has_out = false;
+  bool has_data = false;
+
+  for (int i = 1; i < argc; ++i) {
+    const std::string arg = argv[i];
+    if (arg.compare(0, 3, "in:") == 0) {
+      if (has_in)
+        return 1;
+      in_file = arg.substr(3);
+      has_in = true;
+    } else if (arg.compare(0, 4, "out:") == 0) {
+      if (has_out)
+        return 1;
+      out_file = arg.substr(4);
+      has_out = true;
+    } else if (arg.compare(0, 5, "data:") == 0) {
+      if (has_data)
+        return 1;
+      data_file = arg.substr(5);
+      has_data = true;
+    } else {
+      return 1;
+    }
+  }
+
+  if (!has_data) {
+    return 1;
+  }
+
+  std::ifstream f_data(data_file);
+  if (!f_data.is_open()) {
+    return 2;
+  }
+
+  alisov::Vector< alisov::Meet > meets;
+  alisov::init(meets);
+  std::string line = "";
+  while (std::getline(f_data, line)) {
+    alisov::Meet m;
+    if (alisov::parse_meet(line, m)) {
+      alisov::push_back(meets, m);
+    } else if (!line.empty() && line.find_first_not_of(" \t\r\n") != std::string::npos) {
+      return 3;
+    }
+  }
+  f_data.close();
+}
