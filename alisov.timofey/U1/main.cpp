6b2e27471f83ee1d801331ae0078c5ca1c579531
m0trex <timofeyalisov@gmail.com>
2026-06-25 11:12:23 +0300

    add namespace and vector

--- alisov.timofey/U1/main.cpp
+++ alisov.timofey/U1/main.cpp
@@ -1,11 +1,27 @@
 #include <cstddef>
 #include <iostream>
 
-struct Person
+namespace alisov
 {
-  size_t id;
-  std::string info;
-};
+  struct Person
+  {
+    size_t id;
+    std::string info;
+  };
+
+  template < class T >
+  struct Vector
+  {
+    T *data;
+    size_t size;
+    size_t cap;
+
+    ~Vector()
+    {
+      delete[] data;
+    }
+  };
+}
 
 int main()
 {}
